25e94d8e4656038cf7f3c1bedffa02afcb72bc8e
Jean Carlos Penas <jeanpenas.vixsystem@vixsystem.com.br>
2021-08-24 13:38:17 -0300

    primeiro commit

---
+++ src/main/java/portfolio/appexperiencias/AppExperienciasApplication.java
@@ -0,0 +1,13 @@
+package portfolio.appexperiencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppExperienciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppExperienciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/portfolio/appexperiencias/AppExperienciasApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.appexperiencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppExperienciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
